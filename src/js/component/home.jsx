e02222ae6ce5b0b428b498a5e2822f3e52ea48b6
Jacob Cabot <166416094+JacobBCabot@users.noreply.github.com>
2024-06-22 00:52:39 +0000

    added functions, and 2 fetches

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,51 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 //create your first component
 const Home = () => {
 	const[inputValue, setInputValue] = useState("")
 	const [todoList, setTodoList] = useState([])
 	console.log(todoList)
+function initialFetch(){
+	fetch('https://playground.4geeks.com/todo/users/jacobbcabot')
+	.then(response => {
+		console.log(response)
+		if(!response.ok){
+			fetch('https://playground.4geeks.com/todo/users/jacobbcabot',{
+				method: "POST",
+      			body: JSON.stringify(todoList),
+      			headers: {
+        			"Content-Type": "application/json"
+							}
+		})
+				.then(response => {
+					console.log(response, "response from post request")
+					return response.json()
+				})
+	}
+		return response.json()
+	})
+	.then(data => {
+		console.log(data)
+		setTodoList(data.todos)
+	})
+}
+const createTodo = () => {
+	fetch('https://playground.4geeks.com/todo/todos/jacobbcabot',{
+		method: "POST",
+		body: JSON.stringify({
+			"label":inputValue,
+			"is_done": false
+		  }),
+		headers: {"Content-Type":"application/json"}
+
+		
+	})
+	setTodoList([...todoList, {"label":inputValue,
+			"is_done": false}]);
+}
+	useEffect( ()=> {
+		initialFetch()
+	},[])
 	return (
 		<div className="container">
 			<h1>Todo List</h1>
@@ -16,7 +57,10 @@ const Home = () => {
 						value={inputValue}
 						onKeyDown={(e) => {
 							if (e.key === "Enter") {
-								setTodoList(todoList.concat(inputValue));
+								//setInputValue({"label":inputValue,
+								//				"is_done": false})
+								
+								createTodo()
 								setInputValue("");
 							}
 						}}
@@ -24,8 +68,8 @@ const Home = () => {
 				</div>
 					<div className="listItem">
 						{todoList.map((item, index) => (
-							<p>
-								{item}{" "}
+							<p key={index}>
+								{item.label}{" "}
 								<i className="fa-solid fa-trash-can"
 								onClick={() =>
 									setTodoList(
